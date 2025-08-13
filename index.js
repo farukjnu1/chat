cb406fa21d23407d90435f11fa0120a891507461
farukjnu1@gmail.com <farukjnu1@gmail.com>
2025-08-14 04:19:34 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var formidable = require('formidable');
+
+http.listen(5050, () => {
+    console.log('listening on http://localhost:5050');
+});
+app.use('/files', express.static('files'));
+
+app.get('/js/jquery-3.4.1.min.js', (req, res) => {
+    res.redirect('https://code.jquery.com/jquery-3.4.1.min.js');
+});
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/signup.html');
+});
+app.get('/chat', (req, res) => {
+    res.sendFile(__dirname + '/chat.html');
+});
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+app.post('/uploadfile', function (req, res){
+    var strFilePath = '';
+    var form = new formidable.IncomingForm();
+    form.parse(req);
+    form.on('fileBegin', function (name, file){
+        file.path =__dirname + '/files/' + file.name;
+    });
+    form.on('file', function (name, file){
+        strFilePath ='/files/' + file.name;
+        res.send(JSON.stringify({"filePath": strFilePath, "fileName": file.name }));
+    });
+});
